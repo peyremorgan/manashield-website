cbb1214938b0a1d0897aa485abd04ebc897980be
lapinoux02 <paulbnt@free.fr>
2015-08-23 15:47:11 +0200

    retrait des test de dev

--- js/frontpage.js
+++ js/frontpage.js
@@ -21,16 +21,5 @@ angular.module("manashield")
 			"A gravity enabled bubble shooter tetris.", 
 			"You have to pop all the colored tiles by putting 3 or more together. Be carefull, the tiles always go toward the black one !", 
 			["left arrow : clockwise", "right arrow : anti-clockwise", "down arrow : fast fall"])
-	],[
-		$scope.getGameObject("img/jsh.png", 
-			"J S H", 
-			"A clone of the great Game Super Hexagon.", 
-			"You play the little arrow and have to dodge the obstacles coming at you by moving around the central hexagon.", 
-			["left arrow / left click : clockwise", "right arrow / right click : anti-clockwise"]),
-		$scope.getGameObject("img/thex.png", 
-			"T H E X", 
-			"A gravity enabled bubble shooter tetris.", 
-			"You have to pop all the colored tiles by putting 3 or more together. Be carefull, the tiles always go toward the black one !", 
-			["left arrow : clockwise", "right arrow : anti-clockwise", "down arrow : fast fall"])
 	]];
 })
